59002747ff16f84316f413a1ebe6b5c392178753
j4bberwocky <tommaso.cortonesi@gmail.com>
2021-07-26 16:47:41 +0200

    sol

--- src/main/java/it/j4bberwocky/hackerrank/RepeatedString.java
+++ src/main/java/it/j4bberwocky/hackerrank/RepeatedString.java
@@ -4,8 +4,25 @@ package it.j4bberwocky.hackerrank;
 public class RepeatedString {
     
     public static long repeatedString(String s, long n) {
-        // Write your code here
-        return 0L;
+        long aCounter = 0L;
+        for (char c : s.toCharArray()) {
+            if (c == 'a') {
+                aCounter++;
+            }
+        }
+
+        aCounter = n / s.length() * aCounter;
+
+        if (n % s.length() != 0) {
+            String substring = s.substring(0, (int) (n % s.length()));
+            for (char c : substring.toCharArray()) {
+                if (c == 'a') {
+                    aCounter++;
+                }
+            }
+        }
+
+        return aCounter;
     }
 
 }
